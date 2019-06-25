2ea701b74707c1ba3009b0c0abbeae0ec35f177f
GeovaneGraef <geovane.graef@hotmail.com>
2019-06-25 19:47:59 -0300

    Inicio

---
+++ index.js
@@ -0,0 +1,27 @@
+// Importa o EXPRESS para dentro do projeto
+const express = require("express");
+const server = express();
+server.use(express.json());
+
+const projects = [];
+
+// Listar todos os projetos
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+// Listar um único usuário
+server.get("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  return res.json(projects[id]);
+});
+
+// Cadastrar um novo projeto
+server.post("/projects", (req, res) => {
+  const { id, title, tasks } = req.body;
+  const project = { id, title, tasks };
+  projects.push(project);
+  return res.json(projects);
+});
+
+server.listen(3000);
