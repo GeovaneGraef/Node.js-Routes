e40abf683bed09e7d47429ceec7ed76881751e12
GeovaneGraef <geovane.graef@hotmail.com>
2019-06-25 20:58:12 -0300

    2commit

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ server.use(express.json());
 
 const projects = [];
 
+// MiddleWare que verifica se o ID foi informado para cadastro
+function CheckProjectSend(req, res, next) {
+  if (!req.body.id) {
+    return res.status(400).json({ error: "O id do projeto não existe!" });
+  }
+
+  return next();
+}
+
+// MiddleWare que verifica se o ID existe para que seja alterado
+function CheckIdExists(req, res, next) {
+  if (!projects[req.params.id]) {
+    return res
+      .status(400)
+      .json({ error: "O ID informado não foi encontrado!" });
+  }
+
+  return next();
+}
+
 // Listar todos os projetos
 server.get("/projects", (req, res) => {
   return res.json(projects);
@@ -17,12 +37,26 @@ server.get("/projects/:id", (req, res) => {
 });
 
 // Cadastrar um novo projeto
-server.post("/projects", (req, res) => {
+server.post("/projects", CheckProjectSend, (req, res) => {
   const { id, title, tasks } = req.body;
   const project = { id, title, tasks };
   projects.push(project);
   return res.json(projects);
 });
 
-// Segundo commit
+// Alterar projeto
+server.put("/projects/:id", CheckIdExists, (req, res) => {
+  const { tasks } = req.body;
+  const { id } = req.params;
+  projects[id] = { id, tasks };
+  return res.json(projects);
+});
+
+// Deletar Projeto
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  projects.splice(id, 1);
+  return res.json(projects);
+});
+
 server.listen(3000);
